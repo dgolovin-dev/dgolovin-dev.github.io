8fea425527947b5ed3ec7bc5ca730214bda53487
Dmitry Golovin <sheep.gage@gmail.com>
2022-01-09 20:21:04 +0300

    Update comments.js

--- assets/comments.js
+++ assets/comments.js
@@ -1,9 +1,9 @@
 document.write(`
-               <script src="https://utteranc.es/client.js"
-               repo="dgolovin-dev/dgolovin-dev.github.io"
-               issue-term="pathname"
-               theme="github-light"
-               crossorigin="anonymous"
-               async>
-               </script>
+ <script src="https://utteranc.es/client.js"
+   repo="dgolovin-dev/dgolovin-dev.github.io"
+   issue-term="pathname"
+   theme="github-light"
+   crossorigin="anonymous"
+   async>
+ </script>
 `);
