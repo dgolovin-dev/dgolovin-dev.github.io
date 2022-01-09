f5b1433186d117b57e09c99de1804b621b922044
Dmitry Golovin <sheep.gage@gmail.com>
2022-01-09 20:05:42 +0300

    Create comments.js

---
+++ assets/comments.js
@@ -0,0 +1,8 @@
+var s = document.createElement("script");
+s.setAttribute("src","https://utteranc.es/client.js");
+s.setAttribute("repo","dgolovin-dev/dgolovin-dev.github.io");
+s.setAttribute("issue-term","pathname");
+s.setAttribute("theme","github-light");
+s.setAttribute("crossorigin","anonymous");
+s.setAttribute("async",true);
+document.head.appendChild(s);
